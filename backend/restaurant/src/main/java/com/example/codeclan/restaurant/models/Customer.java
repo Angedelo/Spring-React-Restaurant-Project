9bc6b0321a91ec3cae3ebb84377c243a69acbed2
Matt Addison <matthew.addison@talk21.com>
2018-11-02 15:03:53 +0000

    first commit - customer class created

---
+++ backend/restaurant/src/main/java/com/example/codeclan/restaurant/models/Customer.java
@@ -0,0 +1,67 @@
+package com.example.codeclan.restaurant.models;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+
+@Entity
+@Table(name = "customers")
+public class Customer {
+
+    @Id@GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+
+    @Column(name = "name")
+    public String name;
+
+    @Column(name = "visits")
+    public int visits;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = customer, fetch = FetchType.LAZY)
+    private List<Booking> bookings;
+
+    public Customer(Long id, String name, int visits) {
+        this.id = id;
+        this.name = name;
+        this.visits = visits;
+        this.bookings = new ArrayList<Booking>();
+    }
+
+    public Customer() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVisits() {
+        return visits;
+    }
+
+    public void setVisits(int visits) {
+        this.visits = visits;
+    }
+
+    public List<Booking> getBookings() {
+        return bookings;
+    }
+
+    public void setBookings(List<Booking> bookings) {
+        this.bookings = bookings;
+    }
+}
