a7e32237ccb0a1b50fd02bd5d42d3e5a81384dac
Matt Addison <matthew.addison@talk21.com>
2018-11-07 10:47:35 +0000

    adds edit functionality

--- frontend/restaurant/src/components/BookingEditForm.js
+++ frontend/restaurant/src/components/BookingEditForm.js
@@ -3,13 +3,57 @@ import React from 'react'
 class BookingEditForm extends React.Component {
   constructor(props){
     super(props);
+    this.state = {
+      date: props.booking.date,
+      totalGuests: props.booking.totalGuests,
+      customer: props.booking.customer,
+      table: props.booking.table
+    }
+
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
 
-  render(){
-    return(
-      <div>this is a form still</div>
-    )
+ handleSubmit(event){
+    event.preventDefault();
+    const booking = {
+        "date": this.state.date,
+        "totalGuests": this.state.totalGuests,
+        "customer": this.state.customer,
+        "tabel": this.state.table
+      }
+    this.props.handleBookingEdit(booking)
   }
+
+
+render(){
+
+     const tableOptions = this.props.tables.map((table, index) => {
+      return <option key={index} value={table._links.self.href}>{table.number}</option>
+    })
+
+    const customerOptions = this.props.customers.map((customer, index) => {
+      return <option key={index} value={customer._links.self.href}>{customer.name}</option>
+    })
+
+
+  return (
+    <div>
+      <h3>Edit details</h3>
+      <form onSubmit={this.handleSubmit}>
+        <input type="text" value = {this.state.date} name="date" onChange={event => this.setState({ date: event.target.value })}/>
+        <input type="number" value = {this.state.totalGuests} name="totalGuests" onChange={event => this.setState({ totalGuests: event.target.value })}/>
+        <select name="table" onChange={event => this.setState({ table: event.target.value })}>
+          {tableOptions}
+        </select>
+        <select name="customer" onChange={event => this.setState({customer: event.target.value})} >
+          {customerOptions}
+        </select>
+        <button type="submit">Save</button>
+      </form>
+    </div>
+
+)
 }
+  }
 
 export default BookingEditForm;
